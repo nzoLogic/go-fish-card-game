2041a9adf73f5a9b75d2bff663a0207adba58848
nzoLogic <nzoLogic@gmail.com>
2017-01-20 16:09:13 -0800

    gofish

--- go_fish.rb
+++ go_fish.rb
@@ -1,9 +1,31 @@
+RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+SUITS = ["C", "D", "H", "S"]
+SORTED_CARDS = [
+    "AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S",
+    "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S",
+    "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S",
+    "10C", "10D", "10H", "10S", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS",
+    "KC", "KD", "KH", "KS"
+]
+
 class PlayingCard
   # initialize
+  attr_reader :rank, :suit, :face, :to_s
+
+  def initialize(hash)
+    @rank = hash[:rank]
+    @suit = hash[:suit]
+    @face = @rank + @suit
+    @to_s = @face
+  end
 end
 
-class CardDeck
+class CardDeck < PlayingCard
   # initialize
+  attr_reader :cards
+  def initialize(*bool)
+    @card = Array.new
+  end
 end
 
 class HandOfCards
